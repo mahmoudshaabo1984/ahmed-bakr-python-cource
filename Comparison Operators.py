07ef9d3ebd30f9269140202d6e337efb8a56ff77
mahmoudshaabo1984 <124743406+mahmoudshaabo1984@users.noreply.github.com>
2024-12-30 09:52:42 +0000

    Add files via upload

---
+++ Comparison Operators.py
@@ -0,0 +1,10 @@
+# أمثلة على Comparison Operators
+print("\nأمثلة على Comparison Operators:")
+a, b = 10, 20
+print("a:", a, "b:", b)
+print("a == b:", a == b)  # مقارنة التساوي
+print("a != b:", a != b)  # مقارنة عدم التساوي
+print("a > b:", a > b)    # أكبر من
+print("a < b:", a < b)    # أصغر من
+print("a >= b:", a >= b)  # أكبر من أو يساوي
+print("a <= b:", a <= b)  # أصغر من أو يساوي")]}
