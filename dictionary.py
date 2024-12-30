9d7cccb378bdfbe2c5f4ae6284409262d8ac28df
mahmoudshaabo1984 <124743406+mahmoudshaabo1984@users.noreply.github.com>
2024-12-30 09:54:22 +0000

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,76 @@
+# user ={
+# 'name': 'AhmedBakr',
+# 'age': 25,
+# 'country': 'Egypt',
+# 'City': 'sohag',
+# 'skills': ['html', 'css', 'php'],
+# 'Rating': 10.5,
+# }
+# print(user)
+# print(user['age'])
+# print(user['skills'][1])
+
+
+# tecwindow_team = {
+# 'user1': {
+# 'name': 'Mahmoud Atif',
+# 'age': 25,
+# 'Repetition': 'Python developer',
+# 'Skills': ['html', 'css', 'php', 'python', 'sql',],
+# 'Favorite language.': 'python',
+# },
+# 'user2': {
+# 'name': 'AhmedBakr',
+# 'age': 25,
+# 'Repetition': 'Android developer',
+# 'Skills': ['html', 'css', 'php', 'dart', 'sql',],
+# 'Favorite language.': 'dart',
+# },
+
+# 'user3': {
+# 'name': 'QaisAlrefai',
+# 'age': 24,
+# 'Repetition': 'web developer',
+# 'Skills': ['html', 'css', 'php', 'wordpress', 'sql',],
+# 'Favorite language.': 'php',
+# },
+# }
+
+# print(tecwindow_team)
+# print(tecwindow_team['user1'])
+# print(tecwindow_team['user2'])
+# print(tecwindow_team['user3'])
+
+
+
+
+user1 = {
+'name': 'Mahmoud Atif',
+'age': 25,
+'Repetition': 'Python developer',
+'Skills': ['html', 'css', 'php', 'python', 'sql',],
+'Favorite language.': 'python',
+},
+user2 = {
+'name': 'AhmedBakr',
+'age': 25,
+'Repetition': 'Android developer',
+'Skills': ['html', 'css', 'php', 'dart', 'sql',],
+'Favorite language.': 'dart',
+},
+
+user3 = {
+'name': 'QaisAlrefai',
+'age': 24,
+'Repetition': 'web developer',
+'Skills': ['html', 'css', 'php', 'wordpress', 'sql',],
+'Favorite language.': 'php',
+},
+
+tecwindow = {
+'u1': user1,
+'u2': user2,
+'u3': user3,
+
+}
+print(tecwindow)
